c920eca915b58ca27ee2f382f27b0b0c5c744692
Anastasiya <anastasiaasn@gmail.com>
2024-03-10 19:55:54 +0100

    PROXY-3: Add environment variables - Add a file for environment variables - Import the token and api link into index.js file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import axios from "axios";
+import 'dotenv/config';
+
 import {endOfWeek, format, startOfWeek, subWeeks} from "date-fns";
 
 const DATE_FORMAT = 'yyyy-MM-dd';
 const startDate = format(startOfWeek(subWeeks(new Date(), 1)), DATE_FORMAT);
 const endDate = format(endOfWeek(subWeeks(format(new Date(), DATE_FORMAT), 1)), DATE_FORMAT);
 
-const API_KEY = "NeEFN89c7XJQyGcPLqeq4b34YliftPzW4j2I8CvH";
-const NASA_BASE_API_URL = "https://api.nasa.gov/neo/rest/v1/feed";
+const API_KEY = process.env.API_KEY;
+const NASA_BASE_API_URL = process.env.NASA_BASE_API_URL;
 
 axios.get(NASA_BASE_API_URL, {
     params: {
