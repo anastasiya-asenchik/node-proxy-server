396e7d81f11ae0769100887f6324db0ac240baa8
Anastasiya <anastasiaasn@gmail.com>
2024-03-12 15:50:11 +0100

    PROXY-5: Create a first endpoint - Add endpoint for the example "/meteors" - Output the response in JSON format to endpoint response - test response with browser

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 import 'dotenv/config';
-
 import {endOfWeek, format, startOfWeek, subWeeks} from "date-fns";
 
 const DATE_FORMAT = 'yyyy-MM-dd';
 const startDate = format(startOfWeek(subWeeks(new Date(), 1)), DATE_FORMAT);
-const endDate = format(endOfWeek(subWeeks(format(new Date(), DATE_FORMAT), 1)), DATE_FORMAT);
+const endDate = format(endOfWeek(subWeeks(new Date(), 1)), DATE_FORMAT);
 
 const API_KEY = process.env.API_KEY;
 const NASA_BASE_API_URL = process.env.NASA_BASE_API_URL;
 
-axios.get(NASA_BASE_API_URL, {
+const getAsteroidsData = () => axios.get(NASA_BASE_API_URL, {
     params: {
         start_date: startDate,
         end_date: endDate,
@@ -18,13 +17,11 @@ axios.get(NASA_BASE_API_URL, {
     }
 })
     .then(response => {
-        const asteroidData = response.data;
-
-        const totalAsteroids = asteroidData.element_count;
-
-        console.log(`Number of asteroids seen from ${startDate} to ${endDate}: ${totalAsteroids}`);
-        console.log(JSON.stringify(asteroidData, null, 2));
+        const totalAsteroids = response.data.element_count;
+        return response.data;
     })
     .catch(error => {
         console.error('Error fetching data:', error);
     });
+
+export { getAsteroidsData };
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from "express"
+import { getAsteroidsData } from "./index.js";
 
 const PORT = process.env.PORT;
-const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const app = express();
+app.use(express.json());
+
+app.get('/asteroids', (req, res) => {
+    getAsteroidsData()
+        .then(data => res.send(data))
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            res.status(500).send('Error fetching data');
+        });
 });
 
 app.listen(PORT, () => {
